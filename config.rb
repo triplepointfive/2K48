907bef5422727012c10867a1a9fb55deb7ad2b4b
Ilya Smelkov <ismelkov@restream.rt.ru>
2017-10-03 15:38:20 +0300

    Assets hash 👜

--- config.rb
+++ config.rb
@@ -13,12 +13,5 @@ end
 
 configure :build do
   activate :relative_assets
-
-  activate :external_pipeline,
-    name: :webpack,
-    command: build? ?
-    './node_modules/webpack/bin/webpack.js --bail -p' :
-    './node_modules/webpack/bin/webpack.js --watch -d --progress --color',
-    source: '.tmp/dist',
-    latency: 1
+  activate :asset_hash
 end
